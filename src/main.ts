e05f1fc1f6da795c11c67f30eb693863ddd2fd6f
Christian Carrillo <christiancazu@gmail.com>
2023-10-31 15:46:17 -0500

    setup primevue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/md-dark-deeppurple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+import './assets/styles/main.scss'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.mount('#app')
